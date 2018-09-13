7b2c5bae5b1c2c38667a2738989b7b6030322656
Alexander Stefanov <alexander@mezon.ru>
2018-09-13 00:54:11 +0000

    add stunnel

--- updater.py
+++ updater.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python
 import requests
+import re
 
-headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+headers = {'User-Agent': 'Mozilla/5.0 AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 
-def check_package(package):
+def check_version(package):
     url = "http://github.com/OpenMandrivaAssociation/{package}/raw/master/{package}.spec".format(package=package)
-
     resp = requests.get(url, headers=headers)
     if resp.status_code == 404:
         print('requested package [{}] not found'.format(package))
@@ -13,7 +13,26 @@ def check_package(package):
     if resp.status_code == 200:
         print('requested package [{}] found'.format(package))
         print('trying to detect current version')
-#    print(resp.status_code)
-#    print(resp.content)
+        category_match = re.search('\W*Version[^:]*:(.+)', resp.content.decode('utf-8'))
+        # remove spaces
+        version = category_match.group(1).strip('\t')
+        package_status = {'oma_pkg_ver': '%s' % (float(version)),
+                          'package': '%s' % (str(package))}
+#        print(package_status)
+        print('current version is: %s' % version)
+        return version
 
-check_package('stunnel')
+def check_upstream_stunnel():
+    url = "http://www.stunnel.org/downloads.html"
+    resp = requests.get(url, headers=headers)
+    if resp.status_code == 404:
+        print('requested url [{}] not found'.format(url))
+    if resp.status_code == 200:
+        category_match = re.search('[-][0-9]*[.,][0-9]*', resp.content.decode('utf-8'))
+        # to complicated regex here, just split -
+        upstream_version = category_match.group(0).strip('-')
+        print(upstream_version)
+        return upstream_version
+
+check_version('stunnel')
+check_upstream_stunnel()
