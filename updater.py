61887c7fdd3293680107a7e470edc200457dfdcd
Alexander Stefanov <alexander@mezon.ru>
2018-09-15 15:32:35 +0000

    use power of math

--- updater.py
+++ updater.py
@@ -39,6 +39,7 @@ def check_upstream(package):
         rgx = re.compile(r'git[hl]')
         if rgx.search(upstream_url):
             split_url = upstream_url.split("/")[:-2]
+            project_url = '/'.join(split_url[:5]) + '/'
             apibase = 'https://api.github.com/repos' + '/' + split_url[3] + '/' +  split_url[4] + '/tags'
             github_json = requests.get(apibase, headers=headers)
             data = github_json.json()
@@ -46,20 +47,37 @@ def check_upstream(package):
             version_list = []
             category_match = re.search('\d+(?!.*/).*\d+', project_name)
             upstream_version = category_match.group(0)
-            return upstream_version
+            return upstream_version, project_url
+            # return {'upstream_version': upstream_version, 'project_url': project_url}
         else:
             print("not ready yet")
             exit(0)
 
+def tryint(x):
+    try:
+        return int(x)
+    except ValueError:
+        return x
+
+def splittedname(s):
+    return tuple(tryint(x) for x in re.split('([0-9]+)', s))
+
 def compare_versions(package):
     our_ver = check_version(package)
-    they_ver = check_upstream(package)
-    if sorted(set(str(our_ver))) == sorted(set(str(they_ver))):
-        print("OpenMandriva version of [{0}] is same [{1}] as in upstream [{2}]".format(package, our_ver, they_ver))
-    elif sorted(set(str(our_ver))) < sorted(set(str(they_ver))):
-        print("OpenMandriva version of [{0}] is lower [{1}] than in upstream [{2}]".format(package, our_ver, they_ver))
-    elif sorted(set(str(our_ver))) > sorted(set(str(they_ver))):
-        print("OpenMandriva version of [{0}] is newer [{1}] than in upstream [{2}]".format(package, our_ver, they_ver))
+    upstream_ver, project_url = check_upstream(package)
+    a = splittedname(our_ver)
+    b = splittedname(upstream_ver)
+
+    they_ver = check_upstream(package[:2])
+    if splittedname(our_ver) == splittedname(upstream_ver):
+        print("OpenMandriva version of [{0}] is same [{1}] as in upstream [{2}]".format(package, our_ver, upstream_ver))
+        print("Upstream URL {0}".format(project_url))
+    elif splittedname(our_ver) < splittedname(upstream_ver):
+        print("OpenMandriva version of [{0}] is lower [{1}] than in upstream [{2}]".format(package, our_ver, upstream_ver))
+        print("Upstream URL {0}".format(project_url))
+    elif splittedname(our_ver) > splittedname(upstream_ver):
+        print("OpenMandriva version of [{0}] is newer [{1}] than in upstream [{2}]".format(package, our_ver, upstream_ver))
+        print("Upstream URL {0}".format(project_url))
 
 
 def check_upstream_stunnel():
@@ -75,6 +93,7 @@ def check_upstream_stunnel():
         return upstream_version
 
 #check_upstream("stunnel")
+#compare_versions("stunnel")
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
