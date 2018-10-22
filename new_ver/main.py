214f63154a3e3350d1ebfb7eaba56e69a796a886
Alexander Stefanov <alexander@mezon.ru>
2018-10-22 22:33:12 +0000

    more functions

--- new_ver/main.py
+++ new_ver/main.py
@@ -7,26 +7,10 @@ import os
 import json
 import subprocess
 import re
+import tempfile
 
 headers = {'User-Agent': 'Mozilla/5.0 AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.2171.95 Safari/537.36'}
 
-def check_version(package):
-    url = "http://github.com/OpenMandrivaAssociation/{package}/raw/master/{package}.spec".format(package=package)
-    resp = requests.get(url, headers=headers)
-    if resp.status_code == 404:
-        print('requested package [{}] not found'.format(package))
-    if resp.status_code == 200:
-        spec = resp.content.decode('utf-8')
-        names = name_expander.get_nvs(spec)
-#        category_match = re.search('\W*Version[^:]*:(.+)', resp.content.decode('utf-8'))
-#        # remove spaces
-#        version = category_match.group(1).strip()
-#        package_status = {'oma_pkg_ver': '%s' % (str(version)),
-#                          'package': '%s' % (str(package))}
-#        print('current version is: [{}]'.format(version))
-#        print(package_status)
-#        return version
-
-check_version('htop')
+name_expander.check_version('htop')
 
 #get_nvs(spec_path):
--- new_ver/name_expander.py
+++ new_ver/name_expander.py
@@ -1,6 +1,8 @@
 import rpm
 import os
 import sys
+import tempfile
+import requests
 
 def get_nvs(spec):
     try:
@@ -11,11 +13,6 @@ def get_nvs(spec):
        version = rpm.expandMacro("%{version}")
        nvs.append(name)
        nvs.append(version)
-       print(name)
-       print(name)
-       print(name)
-       print(name)
-       print(name)
        for (filename, num, flags) in rpm_spec.sources:
            if num == 0 and flags == 1:
                # prints htop.tar.gz
@@ -24,12 +21,37 @@ def get_nvs(spec):
                # http://mirrors.n-ix.net/mariadb/mariadb-10.3.9/source/mariadb-10.3.9.tar.gz
                # print(filename)
                nvs.append(filename)
-       print(nvs)
        return nvs
     except:
         return None
 
+
+def check_version(package):
+    headers = {'User-Agent': 'Mozilla/5.0 AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.2171.95 Safari/537.36'}
+    url = "http://github.com/OpenMandrivaAssociation/{package}/raw/master/{package}.spec".format(package=package)
+    resp = requests.get(url, headers=headers)
+    temp = tempfile.NamedTemporaryFile(prefix=package, suffix=".spec")
+    if resp.status_code == 404:
+        print('requested package [{}] not found'.format(package))
+    if resp.status_code == 200:
+       spec = resp.content
+       try:
+           spec_path = temp.name
+           # print("Created file is:", temp)
+           # print("Name of the file is:", temp.name)
+           temp.write(spec)
+           temp.seek(0)
+           names = get_nvs(spec_path)
+           name = names[0]
+           version = names[1]
+           source0 = names[2]
+           print(name, version, source0)
+           return name, version, source0
+       finally:
+           temp.close()
+
 #version = get_nvs('/home/omv/mariadb/mariadb.spec')
+#print(version)
 # name
 #print(version[0])
 # version
